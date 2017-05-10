9187d2334d534f4534da860c07cd4a19bdfa41ab
js-ddev <jsd_cnd@yahoo.fr>
2017-05-10 10:19:53 +0200

    tuto2

---
+++ resources/views/article.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+
+<html lang="fr">
+
+<head>
+
+    <meta charset="UTF-8">
+
+    <title>Test de \resources\views\article.php</title>
+
+<body>
+
+    Hello World !
+    <br/>
+    Test de \resources\views\article.blade.php
+    <br />
+    page {{$numero}}
+
+</body>
+
+</html>
